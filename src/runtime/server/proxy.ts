1a3a84a02e46e43defe71c73b76760075d946a92
Dominique BILLET <d.billet@sied70.fr>
2025-05-04 08:50:53 +0200

    log x-plausible-dropped only if debug

--- src/runtime/server/proxy.ts
+++ src/runtime/server/proxy.ts
@@ -3,13 +3,12 @@ import { createError, defineEventHandler, proxyRequest, useRuntimeConfig, getReq
 export default defineEventHandler((event) => {
   const config = useRuntimeConfig(event).plausible;
   try {
-    console.log('[plausible] proxy request', config.apiEndpoint);
     return proxyRequest(event, config.apiEndpoint, {
       headers: {
         'X-Forwarded-For': getRequestIP(event, { xForwardedFor: true }),
       },
       onResponse(event, response) {
-        if (response.headers.has('x-plausible-dropped')) {
+        if (response.headers.has('x-plausible-dropped') && useRuntimeConfig().public.plausible.debug) {
           console.warn('[plausible:dropped]', response.headers.get('x-plausible-dropped'));
         }
       },
